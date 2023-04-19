9cd124550a28949bf9f92370e99449efea07a8aa
kx0101 <liakos.koulaxis@yahoo.com>
2023-04-19 22:16:38 +0300

    .

--- LRUCache.ts
+++ LRUCache.ts
@@ -1,103 +1,99 @@
-class ListNode {
-  key: number;
-  value: number;
-  prev: ListNode | null;
-  next: ListNode | null;
+class ListNode<K, V> {
+    key: K;
+    value: V;
+    prev: ListNode<K, V> | null;
+    next: ListNode<K, V> | null;
 
-  constructor(key: number, value: number) {
-    this.key = key;
-    this.value = value;
-    this.prev = null;
-    this.next = null;
-  }
+    constructor(key: K, value: V) {
+        this.key = key;
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
 }
 
-class LRUCache {
-  capacity: number;
-  map: Map<number, ListNode>;
-  head: ListNode | null;
-  tail: ListNode | null;
+class LRUCache<K, V> {
+    capacity: number;
+    map: Map<K, ListNode<K, V>>;
+    head: ListNode<K, V> | null;
+    tail: ListNode<K, V> | null;
 
-  constructor(capacity: number) {
-    this.capacity = capacity;
-    this.map = new Map();
-    this.head = null;
-    this.tail = null;
-  }
-
-  private remove(node: ListNode) {
-    if (node.prev !== null) {
-      node.prev.next = node.next;
-    } else {
-      this.head = node.next;
+    constructor(capacity: number) {
+        this.capacity = capacity;
+        this.map = new Map();
+        this.head = null;
+        this.tail = null;
     }
 
-    if (node.next !== null) {
-      node.next.prev = node.prev;
-    } else {
-      this.tail = node.prev;
-    }
-  }
+    remove(node: ListNode<K, V>) {
+        if (node.prev !== null) {
+            node.prev.next = node.next;
+        } else {
+            this.head = node.next;
+        }
 
-  private addToHead(node: ListNode) {
-    node.next = this.head;
-    node.prev = null;
-
-    if (this.head !== null) {
-      this.head.prev = node;
-    }
-
-    this.head = node;
-
-    if (this.tail === null) {
-      this.tail = node;
-    }
-  }
-
-  get(key: number): number {
-    if (this.map.has(key)) {
-      const node = this.map.get(key)!;
-
-      this.remove(node);
-      this.addToHead(node);
-
-      return node.value;
+        if (node.next !== null) {
+            node.next.prev = node.prev;
+        } else {
+            this.tail = node.prev;
+        }
     }
 
-    return -1;
-  }
+    addToHead(node: ListNode<K, V>) {
+        node.next = this.head;
+        node.prev = null;
 
-  put(key: number, value: number): void {
-    if (this.map.has(key)) {
-      const node = this.map.get(key)!;
-      node.value = value;
+        if (this.head !== null) {
+            this.head.prev = node;
+        }
 
-      this.remove(node);
-      this.addToHead(node);
-    } else {
-      const node = new ListNode(key, value);
+        this.head = node;
 
-      this.map.set(key, node);
-      this.addToHead(node);
-
-      if (this.map.size > this.capacity) {
-        const nodeToRemove = this.tail!;
-
-        this.remove(nodeToRemove);
-        this.map.delete(nodeToRemove.key);
-      }
+        if (this.tail === null) {
+            this.tail = node;
+        }
     }
-  }
+
+    get(key: K): V | -1 {
+        if (this.map.has(key)) {
+            const node = this.map.get(key)!;
+
+            this.remove(node);
+            this.addToHead(node);
+
+            return node.value;
+        }
+
+        return -1;
+    }
+
+    put(key: K, value: V): void {
+        if (this.map.has(key)) {
+            const node = this.map.get(key)!;
+            node.value = value;
+
+            this.remove(node);
+            this.addToHead(node);
+        } else {
+            const node = new ListNode(key, value);
+
+            this.map.set(key, node);
+            this.addToHead(node);
+
+            if (this.map.size > this.capacity) {
+                const nodeToRemove = this.tail!;
+
+                this.remove(nodeToRemove);
+                this.map.delete(nodeToRemove.key);
+            }
+        }
+    }
 }
 
-const cache = new LRUCache(2);
+const cache = new LRUCache<string, { name: string, age: number }>(100);
 
-cache.put(1, 1);
-cache.put(2, 2);
-console.log(cache.get(1)); // Output: 1
-cache.put(3, 3);
-console.log(cache.get(2)); // Output: -1
-cache.put(4, 4);
-console.log(cache.get(1)); // Output: -1
-console.log(cache.get(3)); // Output: 3
-console.log(cache.get(4)); // Output: 4
+cache.put('Elijah', { name: 'Elijah Koulaxis', age: 22 });
+cache.put('Takis', { name: 'Takis idk', age: 25 });
+
+console.log(cache.get('Elijah')); // {name: 'Elijah Koulaxis', age: 22}
+console.log(cache.get('Takis')); // {name: 'Takis idk', age: 25}
